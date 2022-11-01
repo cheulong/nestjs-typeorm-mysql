8721d28e4f1835c1c204be99e35249253c7e6313
cheulong <searcheulong@gmail.com>
2022-11-01 22:02:05 +0700

    store session

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { Session } from './typeorm/entities/session';
+import { TypeormStore } from 'connect-typeorm';
+import { AppDataSource } from './configs/dataSource';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const sessionRepository = AppDataSource.getRepository(Session);
   app.use(
     session({
       name: 'Nestjs_Session_ID',
@@ -14,6 +18,7 @@ async function bootstrap() {
       cookie: {
         maxAge: 60000,
       },
+      store: new TypeormStore().connect(sessionRepository),
     }),
   );
   app.use(passport.initialize());
